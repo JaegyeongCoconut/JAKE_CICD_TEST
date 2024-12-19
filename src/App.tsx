0fb456216c938bd29211fbb2e99766c09c832868
Jake <76927141+JaegyeongCoconut@users.noreply.github.com>
2024-12-19 14:53:05 +0900

    develop 문구 변경

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>Hello world</h2>
+      <h2>Develop</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
