e9f24a987b0a4f0a244191885152e0ed0fa20ca7
Jake <76927141+JaegyeongCoconut@users.noreply.github.com>
2024-12-26 10:38:49 +0900

    App.tsx 운영, 개발에 따른 코드 수정

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>Hello world</h2>
-      <h2>Main</h2>
+      <h2>{import.meta.env.VITE_BUILD_ENV}</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
